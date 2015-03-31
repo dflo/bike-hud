728928fd1dcceb97b29d1a1410541b3ea72c01ea
Sam Abey <s@ransara.net>
2015-03-31 16:42:50 -0400

    ignore stuff fixed. otherwise would mess up settings at every sync

--- SOFTWARE/EMBEDDED/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/ti/TivaWare_C_Series-2.0.1.11577/driverlib/ccs/Debug/driverlib.lib"
-
