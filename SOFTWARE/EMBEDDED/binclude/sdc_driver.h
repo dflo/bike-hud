7b4d6fd29cb05f8d6a54e53b01d96b60508584af
Sam Abey <s@ransara.net>
2015-04-08 04:49:36 -0400

    SD card init is on the way with working SPI

---
+++ SOFTWARE/EMBEDDED/binclude/sdc_driver.h
@@ -0,0 +1,7 @@
+#ifndef __BINCLUDE_SDC_DRIVER_H__
+#define __BINCLUDE_SDC_DRIVER_H__
+
+void SDC_Init();
+void SDC_spi_test();
+
+#endif
--- SOFTWARE/EMBEDDED/main.c
+++ SOFTWARE/EMBEDDED/main.c
@@ -9,8 +9,9 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
 
-//#include "driverlib/uart.h"
-//#include "utils/uartstdio.h"
+#include "binclude/sdc_driver.h"
+
+uint32_t g_ui32SysClock;
 
 //*****************************************************************************
 //
@@ -24,44 +25,10 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-//*****************************************************************************
-//
-// Blink the on-board LED.
-//
-//*****************************************************************************
 int
 main(void)
 {
-	unsigned long ulPeriod = 500; //20ms (16Mhz / 64pwm_divider / 50)
-	//
-	// Run from the PLL at 120 MHz.
-	//
-	SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);    //Enable control of PWM module 0
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);   //Enable control of GPIO F
-	SysCtlDelay(10);
-
-	// Configure PWM Clock to match system
-	PWMClockSet(PWM0_BASE, PWM_SYSCLK_DIV_1);
-
-	GPIOPinConfigure(GPIO_PF2_M0PWM2);    //Map PF2 to PWM0 G1, OP 2
-	GPIOPinConfigure(GPIO_PF3_M0PWM3);    //Map PF3 to PWM0 G1, OP 3
-
-	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2 | GPIO_PIN_3);    //Configure PF2 PF3 as PWM
-
-	//Configure PWM0 G1 as UP/DOWN counter with no sync of updates
-	PWMGenConfigure(PWM0_BASE, PWM_GEN_1, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
-
-	PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, ulPeriod);    //Set period of PWM0 G1
-
-	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, ulPeriod / 2);    //Set duty cycle of PWM0 G1
-	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, ulPeriod / 2);    //Set duty cycle of PWM0 G1
-
-	PWMOutputState(PWM0_BASE, (PWM_OUT_2_BIT | PWM_OUT_3_BIT), true);    //Enable OP 0,1 on PWM0 G0
-
-	PWMGenEnable(PWM0_BASE, PWM_GEN_1);    //Enable PWM0, G0
-
-	while(1)
-	{
-	}
+	g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
+	SDC_Init();
+	SDC_spi_test();
 }
